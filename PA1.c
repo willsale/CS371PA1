b22de4fb31f5a63fe4f1ad34702b3e6b47e034f9
willsale <wmal225@uky.edu>
2025-03-08 23:54:50 -0500

    Add files via upload

--- PA1.c
+++ PA1.c
@@ -1,3 +1,27 @@
+/*
+# Copyright 2025 University of Kentucky
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+/* 
+Please specify the group members here
+# Student #1: William Alexander
+# Student #2: none :(
+# Student #3: none :(
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,7 +57,9 @@ void *client_thread_func(void *arg) {
     char send_buf[MESSAGE_SIZE] = "ABCDEFGHIJKMLNOP";
     char recv_buf[MESSAGE_SIZE];
     struct timeval start, end;
-    
+
+  // Hint 1: register the "connected" client_thread's socket in the its epoll instance
+    // Hint 2: use gettimeofday() and "struct timeval start, end" to record timestamp, which can be used to calculated RTT.
     for (int i = 0; i < num_requests; i++) {
         gettimeofday(&start, NULL);
         send(data->socket_fd, send_buf, MESSAGE_SIZE, 0);
@@ -54,7 +80,10 @@ void *client_thread_func(void *arg) {
     return NULL;
 }
 
-/* Runs multiple client threads */
+/*
+ * This function orchestrates multiple client threads to send requests to a server,
+ * collect performance data of each threads, and compute aggregated metrics of all threads.
+ */
 void run_client() {
     pthread_t threads[num_client_threads];
     client_thread_data_t thread_data[num_client_threads];
@@ -82,6 +111,9 @@ void run_client() {
     
     long long total_rtt = 0;
     long total_messages = 0;
+
+    // Hint: use thread_data to save the created socket and epoll instance for each thread
+    // You will pass the thread_data to pthread_create() as below
     for (int i = 0; i < num_client_threads; i++) {
         pthread_join(threads[i], NULL);
         total_rtt += thread_data[i].total_rtt;
@@ -113,6 +145,7 @@ void run_server() {
     event.data.fd = server_fd;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event);
     
+    /* Server's run-to-completion event loop */
     while (1) {
         int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
         for (int i = 0; i < nfds; i++) {
@@ -148,3 +181,4 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+
